4edacc4e3a7cc2fbd591760de75f05e85b97539f
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-03-18 00:31:03 +0330

    add: add models

---
+++ src/Models/CommunicationCallback.php
@@ -0,0 +1,12 @@
+<?php
+namespace D3cr33\Communication\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CommunicationCallback extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+}
