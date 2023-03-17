7f226176a144fc7e809f22ce79b45f6bd97d793f
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-03-18 00:36:42 +0330

    update: update communication callbacks

--- Database/Migrations/2023_03_17_205542_create_communication_callbacks_table.php
+++ Database/Migrations/2023_03_17_205542_create_communication_callbacks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('communication_callbacks', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('communication_id');
+            $table->string('callback');
+            $table->json('callback_data');
             $table->timestamps();
         });
     }
