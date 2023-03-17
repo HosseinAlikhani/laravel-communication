a714bd97b2e5bd3f605dab53c5bb11b6be38be3b
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-03-18 01:23:20 +0330

    add: add CommunicationServiceProvider

---
+++ src/CommunicationServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace D3cr33\Communication;
+
+use Illuminate\Support\ServiceProvider;
+
+class CommunicationServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../Config/communication.php', 'communication');
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPublishing();
+        $this->loadRequirements();
+    }
+
+
+    /**
+     * load requirements section
+     */
+    private function loadRequirements()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
+
+        $this->loadTranslationsFrom(__DIR__.'/../Lang', 'communication');
+    }
+
+    /**
+     * register publishing file
+     */
+    private function registerPublishing()
+    {
+        $this->publishes([
+            __DIR__.'/Config/communication.php' => config_path('communication.php'),
+        ], 'communication-config');
+    }
+}
