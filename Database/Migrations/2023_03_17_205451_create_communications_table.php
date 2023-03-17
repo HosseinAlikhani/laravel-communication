add60a68631f171cd3ea715e91056b1fd793829d
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-03-18 00:36:21 +0330

    update: update communicaton migration with columns

--- Database/Migrations/2023_03_17_205451_create_communications_table.php
+++ Database/Migrations/2023_03_17_205451_create_communications_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('communications', function (Blueprint $table) {
             $table->id();
+            $table->integer('service_type');
+            $table->integer('port_type');
+            $table->morphs('model');
+            $table->json('template')->nullable();
+            $table->integer('template_id')->nullable();
+            $table->integer('template_data');
+            $table->timestamp('send_at')->nullable();
+            $table->timestamp('delivery_at')->nullable();
+            $table->integer('try')->default(0);
             $table->timestamps();
         });
     }
