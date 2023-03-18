181f4ac7d1bb6a9e2275b19d2e4b486d7abe498e
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-03-19 01:48:21 +0330

    update: add const for serviceType + portType + initialize method

--- src/Services/Service.php
+++ src/Services/Service.php
@@ -1,7 +1,39 @@
 <?php
-namespace D3cr33\Communication;
+namespace D3cr33\Communication\Services;
+
+use D3cr33\Communication\Config\Config;
+use D3cr33\Communication\Models\Communication;
 
 class Service
 {
-    
+    public const SERVICE_TYPE = [
+        0   =>  'sms',
+        1   =>  'push-notification',
+        2   =>  'email'
+    ];
+
+    public const PORT_TYPE = [
+        0   =>  'smsir',
+        1   =>  'kavehnegar'
+    ];
+
+    /**
+     * store config
+     * @var Config
+     */
+    protected Config $config;
+
+    public function __construct(Communication $communication)
+    {
+        $this->initialize($communication);
+    }
+
+    /**
+     * initialize port service
+     * @param Communication $communication
+     */
+    private function initialize(Communication $communication)
+    {
+        $this->config = new Config($communication->port_type);
+    }
 }
