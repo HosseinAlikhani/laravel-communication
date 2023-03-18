c773541289a13afd58bf1863865bfa803de8d7cf
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-03-19 01:47:16 +0330

    update: add service_type_not_found trans key

--- Lang/fa/messages.php
+++ Lang/fa/messages.php
@@ -2,4 +2,5 @@
 return [
     'port_not_found'    =>  'سرویس انتخاب شده معتبر نمی باشد.',
     'config_not_found'  =>  'تنظیمات برای :port یافت نشد.',
+    'service_type_not_found'    =>  ':serviceType  معتبر نمی باشد.',
 ];
