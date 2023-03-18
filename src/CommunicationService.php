50bf0cb0de081233f9961e9390b85ff6d5bb1ca9
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-03-19 01:49:28 +0330

    [wip] update: add execute method

--- src/CommunicationService.php
+++ src/CommunicationService.php
@@ -3,5 +3,12 @@ namespace D3cr33\Communication;
 
 final class CommunicationService
 {
-
+    /**
+     * execute communication
+     * @param array $communicationData
+     */
+    public function execute(array $communicationData)
+    {
+        // $request = new C
+    }
 }
