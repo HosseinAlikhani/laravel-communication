ca5238d6d2ee03a3f4142aed40fc48912bc52185
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-03-20 19:17:38 +0330

    update: add CommunicationLog model

---
+++ src/Models/CommunicationLog.php
@@ -0,0 +1,14 @@
+<?php
+namespace D3cr33\Communication\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CommunicationLog extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    const UPDATED_AT = null;
+}
