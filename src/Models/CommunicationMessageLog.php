7201869f87d777375ef5d44ceeef3429218f1df8
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-03-20 13:47:40 +0330

    update: rename communicationMessageLog model

--- src/Models/CommunicationMessageLog.php
+++ src/Models/CommunicationMessageLog.php
@@ -4,9 +4,11 @@ namespace D3cr33\Communication\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CommunicationLog extends Model
+class CommunicationMessageLog extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    const UPDATED_AT = null;
 }
