c2ce9e540e9efcb4304ef71a0dc941ca13b89d56
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-03-21 01:14:56 +0330

    update: add config for slack

--- Config/communication.php
+++ Config/communication.php
@@ -4,5 +4,8 @@ return [
 	    "USER_API_KEY"    =>  env("SMSIR_USER_API_KEY", ""),
 	    "SECRET_KEY" =>  env("SMSIR_SECRET_KEY", ""),
         "LINE_NUMBER"   =>  env("SMSIR_LINE_NUMBER", "")
+    ],
+    "SLACK" => [
+        "HOOK_URL" => env("SLACK_HOOK_URL", "")
     ]
 ];
