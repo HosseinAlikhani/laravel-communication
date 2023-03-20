72a6a6b49c8e18f48eca329f684bf333b76a175a
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-03-21 00:27:21 +0330

    [WIP] update SmsirService

--- src/Services/SmsirService.php
+++ src/Services/SmsirService.php
@@ -1,6 +1,7 @@
 <?php
 namespace D3cr33\Communication\Services;
 
+use Exception;
 use Illuminate\Support\Facades\Http;
 
 class SmsirService extends Service
@@ -23,14 +24,55 @@ class SmsirService extends Service
             'Content-Type' => 'application/json',
             'Accept'    =>  'text/plain'
         ])->post('https://RestfulSms.com/api/Token',[
-            'UserApiKey'    =>  '',
-            'SecretKey' =>  ''
+            'UserApiKey'    =>  $this->config->USER_API_KEY,
+            'SecretKey' =>  $this->config->SECRET_KEY
         ]);
 
-        if( $response->failed() ){
-            return;
+        if(! $response->json('IsSuccessful') ){
+            $this->responseTranslate($response->json());
+            $this->log();
+            return false;
         }
 
         $this->token = $response->json('TokenKey');
+        throw new Exception($this->token);
+        return $this->token;
+        return true;
+    }
+
+    protected function send()
+    {
+        return $this->generateToken();
+        if(! $this->generateToken() ){
+            return false;
+        }
+
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'Accept' => 'text/plain',
+            'X-API-KEY' => $this->token
+        ])->post('https://api.sms.ir/v1/send/likeToLike', [
+            'messageTexts'  =>  [
+                'its test',
+            ],
+            'mobiles'   =>  [
+                '09361374744'
+            ]
+        ]);
+
+    }
+
+    /**
+     * translate response
+     * @var array $response
+     * @return
+     */
+    protected function responseTranslate(array $response)
+    {
+        $this->setResponse([
+            'status'    =>  $response['IsSuccessful'],
+            'statusCode' => $response['IsSuccessful'] ? 200 : 500,
+            'message'   =>  $response['Message']
+        ]);
     }
 }
