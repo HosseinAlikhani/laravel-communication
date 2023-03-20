b983f9f6cee9a6cfe056e4609b00f2c9abd5152b
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-03-20 18:22:37 +0330

    update: rename some variable

--- Lang/fa/messages.php
+++ Lang/fa/messages.php
@@ -2,8 +2,8 @@
 return [
     'port_not_found'    =>  'سرویس انتخاب شده معتبر نمی باشد.',
     'config_not_found'  =>  'تنظیمات برای :service یافت نشد.',
-    'service_type_not_found'    =>  'شناسه سرویس :serviceType معتبر نمی باشد.',
-    'port_type_not_found'   =>  'شناسه پورت :portType معتبر نمی باشد.',
+    'service_type_not_found'    =>  'شناسه سرویس :service معتبر نمی باشد.',
+    'port_type_not_found'   =>  'شناسه پورت :port معتبر نمی باشد.',
     'template_not_found'    =>  'شما باید یکی از مقادیر template یا templateId را مقداردهی نمایید.',
     'thread_not_found'  =>  'شناسه نحوه پاسخ :thread معتبر نمی باشد.',
     'callback_data_not_found'   =>  'مقدار بازگشتی برای :callback وارد نشده است.',
