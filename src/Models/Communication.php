dda9691c3ef5f8ac370f37fde7bd1734b44f50eb
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-03-20 13:47:53 +0330

    update: add static method makeTemplateReplacement

--- src/Models/Communication.php
+++ src/Models/Communication.php
@@ -3,6 +3,7 @@ namespace D3cr33\Communication\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Communication extends Model
 {
@@ -14,4 +15,28 @@ class Communication extends Model
         'template_data' =>  'array',
         'receiver_data' =>  'array',
     ];
+
+    public static function makeTemplateReplacement(Communication $communication)
+    {
+        if(! $communication->template ){
+            return null;
+        }
+
+        foreach ($communication->template_data as $key => $value) {
+            $shouldReplace[':'.Str::ucfirst($key ?? '')] = Str::ucfirst($value ?? '');
+            $shouldReplace[':'.Str::upper($key ?? '')] = Str::upper($value ?? '');
+            $shouldReplace[':'.$key] = $value;
+        }
+
+        $line = strtr($communication->template, $shouldReplace);
+        $communication->message()->create([
+            'message'   =>  $line
+        ]);
+        return $line;
+    }
+
+    public function message()
+    {
+        return $this->hasOne(CommunicationMessageLog::class);
+    }
 }
