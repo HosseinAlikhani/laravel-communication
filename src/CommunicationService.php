42e70ead02668ac78ce29c19de148e856c2cb3e6
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-03-20 16:24:28 +0330

    [WIP]: update: update execute method

--- src/CommunicationService.php
+++ src/CommunicationService.php
@@ -1,6 +1,9 @@
 <?php
 namespace D3cr33\Communication;
 
+use D3cr33\Communication\Models\Communication;
+use D3cr33\Communication\Requests\CommunicationRequest;
+
 final class CommunicationService
 {
     /**
@@ -9,6 +12,10 @@ final class CommunicationService
      */
     public function execute(array $communicationData)
     {
-        // $request = new C
+        $request = new CommunicationRequest($communicationData);
+
+        $communication = Communication::createCommunication($request);
+
+        $communication->makePort();
     }
 }
