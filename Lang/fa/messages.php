c95d7929319abfeef83e676466853057fd42d53c
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-03-20 16:24:04 +0330

    update: update config_not_found trans key

--- Lang/fa/messages.php
+++ Lang/fa/messages.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'port_not_found'    =>  'سرویس انتخاب شده معتبر نمی باشد.',
-    'config_not_found'  =>  'تنظیمات برای :port یافت نشد.',
+    'config_not_found'  =>  'تنظیمات برای :service یافت نشد.',
     'service_type_not_found'    =>  'شناسه سرویس :serviceType معتبر نمی باشد.',
     'port_type_not_found'   =>  'شناسه پورت :portType معتبر نمی باشد.',
     'template_not_found'    =>  'شما باید یکی از مقادیر template یا templateId را مقداردهی نمایید.',
