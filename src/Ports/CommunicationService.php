2cfec540b811665a621e15b5f549a468f2e63739
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-04-20 12:21:42 +0330

    update: add comment in CommunicationService class

--- src/Ports/CommunicationService.php
+++ src/Ports/CommunicationService.php
@@ -9,6 +9,18 @@ final class CommunicationService
     /**
      * execute communication
      * @param array $communicationData
+     * @param string $$communicationData[service]
+     * @param string $$communicationData[port]
+     * @param string $$communicationData[model_type]
+     * @param string $$communicationData[model_id]
+     * @param string $$communicationData[template]
+     * @param string $$communicationData[template_id]
+     * @param string $$communicationData[template_data]
+     * @param string $$communicationData[receiver_data]
+     * @param string $$communicationData[end_at]
+     * @param string $$communicationData[thread]
+     * @param string $$communicationData[callback]
+     * @param string $$communicationData[callback_data]
      */
     public function execute(array $communicationData)
     {
