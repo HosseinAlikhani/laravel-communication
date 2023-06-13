9d3e6bf34a4ff92a0eced057f1112c4424d4172f
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-06-13 20:56:50 +0330

    add: add TestCase on communication module

---
+++ tests/TestCase.php
@@ -0,0 +1,36 @@
+<?php
+namespace D3cr33\Communication\Test;
+
+use D3cr33\Communication\CommunicationServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+class TestCase extends OrchestraTestCase
+{
+    /**
+     * store wallet faker
+     */
+    protected CommunicationFaker $faker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->faker = app(CommunicationFaker::class);
+        $this->artisan('migrate', ['--database' => 'testbench'])->run();
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [CommunicationServiceProvider::class];
+    }
+}
