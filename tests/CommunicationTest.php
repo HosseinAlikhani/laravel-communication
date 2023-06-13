977f24d4ac4e672be5c0bad229460940c4e4ca89
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-06-13 20:21:25 +0330

    add: add communicationTest class

---
+++ tests/CommunicationTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace D3cr33\Communication\Test;
+
+use Tests\TestCase;
+
+class CommunicationTest extends TestCase
+{
+    public function test_communicaton_execute_method_success()
+    {
+        dd('test one');
+    }
+}
