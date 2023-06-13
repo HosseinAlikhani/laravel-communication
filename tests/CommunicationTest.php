b851e730fa46aee568981e5237dfdfbda3ccea99
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-06-13 21:13:37 +0330

    update: update test_communicaton_execute_method_success method

--- tests/CommunicationTest.php
+++ tests/CommunicationTest.php
@@ -1,12 +1,23 @@
 <?php
 namespace D3cr33\Communication\Test;
 
-use Tests\TestCase;
-
 class CommunicationTest extends TestCase
 {
     public function test_communicaton_execute_method_success()
     {
-        dd('test one');
+        $data = [
+            'service'   =>  2,
+            'port'  =>  1,
+            'model_type'    =>  $this->faker->modelType(),
+            'model_id'  =>  $this->faker->modelId(),
+            'template'  =>  $this->faker->template(),
+            'template_id'   =>  $this->faker->templateId(),
+            'template_data' =>  $this->faker->templateData(),
+            'receiver_data' =>  $this->faker->receiverData(),
+            'send_at'   =>  $this->faker->sendAt(),
+            'thread'    =>  $this->faker->thread(),
+            'callback'  =>  $this->faker->callback(),
+            'callback_data' =>  $this->faker->callbackData()
+        ];
     }
 }
