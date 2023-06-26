dad4df1ebc43f550a0652537aae32b3c3c497cf9
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-06-26 23:00:16 +0330

    update: update response for en/messages

--- Lang/en/messages.php
+++ Lang/en/messages.php
@@ -1,4 +1,15 @@
 <?php
 return [
+    'port_not_found'    =>  'Port not found ..!',
+    'config_not_found'  =>  'Config not found for :service.',
+    'service_type_not_found'    =>  ':service type not found.',
+    'port_type_not_found'   =>  ':port type not found.',
+    'template_not_found'    =>  'You must fill template or templateId.',
+    'thread_not_found'  =>  ':thread not valid ..!',
+    'callback_data_not_found'   =>  'callback data not entered for :callback.',
+    'port_not_support'  =>  ':port not valid for :service',
 
+    'request_submited'  =>  'Request submited',
+    'response_success'  =>  'Response success',
+    'response_failed'   =>  'Please try again later ..!'
 ];
