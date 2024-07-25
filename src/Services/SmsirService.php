04bedb8e78445dcb4659c78e81ebb4278972906a
Hossein Alikhani <hossein.alikhani1374@gmail.com>
2024-07-26 01:35:43 +0330

    fix: fix bug

--- src/Services/SmsirService.php
+++ src/Services/SmsirService.php
@@ -75,11 +75,11 @@ class SmsirService extends Service
             if( $this->response->isSuccessful() ){
                 $this->communicationDelivered();
             }else {
-                $this->log();
                 $this->responseTranslate([
                     'success'    =>  false,
                     'message'   =>  $this->response->message
                 ]);
+                $this->log();
                 throw new Exception($this->response->message);
             }
             return $this->response->toArray();
@@ -112,11 +112,11 @@ class SmsirService extends Service
             if( $this->response->isSuccessful() ){
                 $this->communicationDelivered();
             }else{
-                $this->log();
                 $this->responseTranslate([
                     'success'    =>  false,
                     'message'   =>  $this->response->message
                 ]);
+                $this->log();
                 throw new Exception($this->response->message);
             }
             return $this->response->toArray();
